2b867fa115ce1e370559613db59af928b6659cde
Sandun Madhushan <sandunhmadushan9330@gmail.com>
2025-08-12 21:12:54 +0530

    Standardize quote usage in imports and CSS
    Replaced single quotes with double quotes in TypeScript and CSS import statements for consistency. Also fixed minor whitespace issues in CSS class definitions.

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,8 @@
-export { Header } from './Header';
-export { RepoInput } from './RepoInput';
-export { TemplateSelector } from './TemplateSelector';
-export { EditorSection } from './EditorSection';
-export { PreviewSection } from './PreviewSection';
-export { ExportOptions } from './ExportOptions';
-export { FeatureCard } from './FeatureCard';
-export { Footer } from './Footer';
+export { Header } from "./Header";
+export { RepoInput } from "./RepoInput";
+export { TemplateSelector } from "./TemplateSelector";
+export { EditorSection } from "./EditorSection";
+export { PreviewSection } from "./PreviewSection";
+export { ExportOptions } from "./ExportOptions";
+export { FeatureCard } from "./FeatureCard";
+export { Footer } from "./Footer";
